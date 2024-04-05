8da772d25ed4e5fc06be0407d540a0603b0eedec
criptorockstar <criptorockstar@gmail.com>
2024-04-05 01:32:33 -0300

    [chore] navigation implemented

--- logic/podcast_scraper.py
+++ logic/podcast_scraper.py
@@ -1,9 +1,12 @@
+from time import sleep
+
 from libs.web_scraping import WebScraping
 
 
 class PodcastScraper(WebScraping):
     def __init__(self, headless: bool, urls: str) -> None:
         """Starts Chrome and initializes the WebScraping class.
+
         Args:
             headless: (bool, optional) if true the browser will
             not be shown. Defaults to False.
@@ -19,18 +22,43 @@ class PodcastScraper(WebScraping):
         self.header_removed = False
 
         # Urls
-        self.urls = urls
+        self.urls = urls.split("\n")
 
         # Store data
         self.extracted_data = {}
 
-    def podcast_urls(self) -> None:
-        """Loop through a CSV and extract the links to use later
-        Args:
-            csv_urls: (csv) csv containing podcast links
+    def __get_podcast__(self) -> str:
+        """Extract a URL
+
+        Returns: (String) podcast url.
         """
-        print(self.urls)
+
+        # Get first value from the list
+        url = self.urls[0]
+
+        # Remove the readed value
+        self.urls.pop(0)
+
+        return url
+
+    def __loop_podcast__(self, url) -> None:
+        """Loop through one podcast and extract its content
+
+        Args: (String) podcast's url.
+        """
+
+        # CSS selectors
+        selectors = {}
 
     def extract_podcast(self):
         """Extracts podcast data"""
-        pass
+
+        podcasts = len(self.urls)
+
+        for _ in range(0, podcasts):
+
+            # Set podcast's url
+            url = self.__get_podcast__()
+
+            # Loop and extract content
+            self.__loop_podcast__(url)
